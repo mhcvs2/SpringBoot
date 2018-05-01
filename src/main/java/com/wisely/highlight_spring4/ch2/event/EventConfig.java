b02f1e960f4249564f2a89bda914095e5c5c5f8c
mhcvs2 <merzhong0501@163.com>
2018-05-01 17:04:31 +0800

    add

---
+++ src/main/java/com/wisely/highlight_spring4/ch2/event/EventConfig.java
@@ -0,0 +1,10 @@
+package com.wisely.highlight_spring4.ch2.event;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class EventConfig {
+
+}
