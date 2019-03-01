c796fe6f3a89260bfce05ca7bcfea0e59cdaf3c5
MAHONGCHAO [马洪超] <MAHONGCHAO@kingsoft.cn>
2019-03-01 17:37:37 +0800

    add

--- scheduled_test/src/main/java/com/mhc/scheduled_test/TestConfig.java
+++ scheduled_test/src/main/java/com/mhc/scheduled_test/TestConfig.java
@@ -15,22 +15,37 @@ public class TestConfig implements SchedulingConfigurer {
     @Autowired
     ScheduleConfig scheduleConfig;
 
+//    @Override
+//    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+//        scheduledTaskRegistrar.addTriggerTask(
+//                //1.添加任务内容(Runnable)
+//                () -> System.out.println("执行定时任务2: " + LocalDateTime.now().toLocalTime()),
+//                //2.设置执行周期(Trigger)
+//                triggerContext -> {
+//                    //2.1 从数据库获取执行周期
+//                    String cron = scheduleConfig.getCron();
+//                    //2.2 合法性校验.
+//                    if (StringUtils.isEmpty(cron)) {
+//                        // Omitted Code ..
+//                    }
+//                    //2.3 返回执行周期(Date)
+//                    return new CronTrigger(cron).nextExecutionTime(triggerContext);
+//                }
+//        );
+//    }
+
     @Override
     public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        TestTask1 task1 = new TestTask1("task1");
         scheduledTaskRegistrar.addTriggerTask(
-                //1.添加任务内容(Runnable)
-                () -> System.out.println("执行定时任务2: " + LocalDateTime.now().toLocalTime()),
-                //2.设置执行周期(Trigger)
-                triggerContext -> {
-                    //2.1 从数据库获取执行周期
-                    String cron = scheduleConfig.getCron();
-                    //2.2 合法性校验.
-                    if (StringUtils.isEmpty(cron)) {
-                        // Omitted Code ..
-                    }
-                    //2.3 返回执行周期(Date)
-                    return new CronTrigger(cron).nextExecutionTime(triggerContext);
-                }
+                task1,
+                triggerContext -> new CronTrigger(scheduleConfig.getCron()).nextExecutionTime(triggerContext)
+        );
+        TestTask1 task2 = new TestTask1("task2");
+        scheduledTaskRegistrar.addTriggerTask(
+                task2,
+                triggerContext -> new CronTrigger(scheduleConfig.getCron()).nextExecutionTime(triggerContext)
         );
     }
+
 }
