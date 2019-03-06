10e37a5e8f849ddb0ec1cb780fb054e5b215d93f
MAHONGCHAO [马洪超] <MAHONGCHAO@kingsoft.cn>
2019-03-06 15:22:05 +0800

    add

---
+++ mybatis2/readme.txt
@@ -0,0 +1,3 @@
+点击菜单run -> edit configurations -> maven -> command line  填写 mybatis-generator:generate -e
+
+working directory选项目所在路径
--- mybatis2/src/main/java/com/mhc/mybatis2/mapper/UserMapper.java
+++ mybatis2/src/main/java/com/mhc/mybatis2/mapper/UserMapper.java
@@ -1,11 +1,22 @@
 package com.mhc.mybatis2.mapper;
 
 import com.mhc.mybatis2.model.User;
-import org.springframework.stereotype.Component;
+import com.mhc.mybatis2.model.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
-@Component
 public interface UserMapper {
+    int countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
     int insert(User record);
 
     int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
 }
