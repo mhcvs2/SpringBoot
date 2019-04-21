704d82a0f7ad6974d257a1103778a9d45814072b
MAHONGCHAO [马洪超] <MAHONGCHAO@kingsoft.cn>
2019-04-21 16:46:51 +0800

    add

---
+++ condition_test/src/main/java/com/mhc/condition_test/condition/TestCondition1.java
@@ -0,0 +1,13 @@
+package com.mhc.condition_test.condition;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class TestCondition1 implements Condition {
+
+    @Override
+    public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
+        return true;
+    }
+}
