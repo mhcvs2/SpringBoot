02d11e9372d88b250886c0816ad27413f1e41a46
mhcvs2 <merzhong0501@163.com>
2020-01-31 13:14:15 +0800

    add

---
+++ spring_cloud/springcloud_gateway/provider/src/main/java/com/mhc/test/provider/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.mhc.test.provider.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(value = "time", method = RequestMethod.GET)
+    public String hello(HttpServletRequest request){
+
+        return "hello, "
+                + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())
+                + ", extendtag ["
+                + request.getHeader("extendtag")
+                + "]";
+    }
+
+}
